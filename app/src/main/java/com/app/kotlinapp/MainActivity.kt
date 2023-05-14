80ade8815477c4525bdf17a333e09a0fa7f102d4
FreddyRobert17 <freddy.bedregal.ticona@gmail.com>
2023-05-14 12:51:15 -0500

    feat: add file provider

--- app/src/main/java/com/app/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/app/kotlinapp/MainActivity.kt
@@ -3,14 +3,19 @@ package com.app.kotlinapp
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.core.graphics.drawable.toBitmap
 import com.app.kotlinapp.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     private lateinit var heroImage: ImageView
@@ -18,7 +23,10 @@ class MainActivity : AppCompatActivity() {
     private var picturePath = ""
 
     private val getContent = registerForActivityResult(ActivityResultContracts.TakePicture()){
-
+        success ->
+        if(success && picturePath.isNotEmpty()){
+            heroBitmap = BitmapFactory.decodeFile(picturePath)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,8 +46,24 @@ class MainActivity : AppCompatActivity() {
 
         heroImage = binding.heroImage
         binding.heroImage.setOnClickListener {
-
+            openCamera()
         }
+    }
+
+    private fun openCamera(){
+        val file = createImageFile()
+        FileProvider.getUriForFile(this, "$packageName.provider", file)
+    }
+
+    private fun createImageFile(): File {
+        val fileName = "superhero_image"
+        val fileDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        Log.i("TAG", fileDirectory.toString())
+        val file = File.createTempFile(fileName, ".jpg", fileDirectory)
+        Log.i("TAG",file.toString())
+        picturePath = file.absolutePath
+        Log.i("TAG",file.absolutePath.toString())
+        return file
     }
 
     private fun openDetailActivity(hero: Hero) {
